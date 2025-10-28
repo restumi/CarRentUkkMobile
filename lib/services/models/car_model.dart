eee194b102c8b0819a9d464930c366fb260a9997
restumi <maulanaismael5@gmail.com>
2025-10-28 21:03:19 +0700

    change car model with data from API

--- lib/services/models/car_model.dart
+++ lib/services/models/car_model.dart
@@ -1,13 +1,34 @@
 class Car {
+  final String id;
   final String name;
-  final String price;
-  final String type;
+  final String brand;
+  final String seat;
+  final String pricePerDay;
   final String image;
+  final String createdAt;
+  final String updatedAt;
 
   Car({
+    required this.id,
     required this.name,
-    required this.price,
-    required this.type,
+    required this.brand,
+    required this.seat,
+    required this.pricePerDay,
     required this.image,
+    required this.createdAt,
+    required this.updatedAt,
   });
+
+  factory Car.fromJson(Map<String, dynamic> json) {
+    return Car(
+      id: json['id'],
+      name: json['name'],
+      brand: json['brand'],
+      seat: json['seat'],
+      pricePerDay: json['price_per_day'],
+      image: json['image'],
+      createdAt: json['created_at'],
+      updatedAt: json['updated_at'],
+    );
+  }
 }
